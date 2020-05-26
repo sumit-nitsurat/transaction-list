5b1539e28bb7bcfe575bca97977ace55da992c81
Sumit Chaudhari <sumit@cryptorai.com>
2020-05-26 22:56:53 +0800

    router.js updated

--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
 import React, { Suspense, lazy } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-import AccountList from "./accountList"
-import TransactionList from "./transactionList"
-import TransactionDetail from "./transactionDetail"
+const AccountList =  lazy(() => import("./accountList"))
+const TransactionList =  lazy(() => import("./transactionList"))
+const TransactionDetail =  lazy(() => import("./transactionDetail"))
 export const Routes = () => (
   <Router>
-    {/* <Suspense fallback={<div>Loading...</div>}> */}
+    <Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Route exact path="/" component={AccountList} />
         <Route exact path="/:accountNumber" component={TransactionList} />
         <Route path="/:accountNumber/:transactionId" component={TransactionDetail} />
       </Switch>
-    {/* </Suspense> */}
+    </Suspense>
   </Router>
 )
